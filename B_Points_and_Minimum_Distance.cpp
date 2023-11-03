5fe68e25bf324cbfd7139b5874bd879bd62b8160
samarthcode1 <nagsamarth3@gmail.com>
2023-11-04 00:46:31 +0530

    commit

---
+++ B_Points_and_Minimum_Distance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+ int t; cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    int arr[2*n];
+    for(int i=0;i<2*n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+2*n);
+    cout<<arr[n-1]-arr[0]+arr[2*n-1]-arr[n]<<endl;
+    for(int i=0,j=n;i<n;i++,j++){
+        cout<<arr[i]<<" "<<arr[j]<<endl;
+    }
+  }
+    return 0;
+}
