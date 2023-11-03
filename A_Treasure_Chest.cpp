5788904f19b9c651f6a03405c7dbed9d3eaf035b
samarthcode1 <nagsamarth3@gmail.com>
2023-11-03 23:50:27 +0530

    commit

---
+++ A_Treasure_Chest.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ int t; cin>>t;
+  while(t--){
+      
+      int x,y,k;
+      cin>>x>>y>>k;
+      int t=x;
+      if(y<=x)
+        cout<<t<<"\n";
+      else{
+         if(y-x<k){
+            cout<<y<<"\n";
+         }else{
+          t+=k;
+          t+=2*(y-t);
+          cout<<t<<"\n";
+          }
+      }
+        
+  }
+    return 0;
+}
