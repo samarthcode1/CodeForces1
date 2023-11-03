76c42f946eb467a956f6ded1d274a18fcdfa30e8
samarthcode1 <nagsamarth3@gmail.com>
2023-11-04 00:54:49 +0530

    commit

--- B_Points_and_Minimum_Distance.cpp
+++ B_Points_and_Minimum_Distance.cpp
@@ -13,9 +13,9 @@ int main() {
     }
     sort(arr,arr+2*n);
     cout<<arr[n-1]-arr[0]+arr[2*n-1]-arr[n]<<endl;
-    for(int i=0,j=n;i<n;i++,j++){
-        cout<<arr[i]<<" "<<arr[j]<<endl;
-    }
+    for(int i = 0, j = n; i < n; i++, j++){
+		cout<< arr[i] << " " << a[j] <<"\n";
+	}
   }
     return 0;
 }
