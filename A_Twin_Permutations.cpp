4270fd092ec5f170065b3ee996fdc00cbcc53a70
samarthcode1 <nagsamarth3@gmail.com>
2024-01-13 15:55:23 +0530

    commit

---
+++ A_Twin_Permutations.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+const int mod = 1000000007;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+            cout<<n+1-v[i]<<" ";
+        }
+        cout<<endl;
+        // int maxi=*max_element(v.begin(),v.end());
+        // cout
+    }
+    return 0;
+}
